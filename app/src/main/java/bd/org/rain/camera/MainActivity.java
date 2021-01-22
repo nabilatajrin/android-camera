97c15306d587805c0c5bc86d9dc60e3ff112c903
Nabila Tajrin <nabilatajrin@users.noreply.github.com>
2021-01-22 11:09:09 +0600

    Update MainActivity.java

--- app/src/main/java/bd/org/rain/camera/MainActivity.java
+++ app/src/main/java/bd/org/rain/camera/MainActivity.java
@@ -3,17 +3,14 @@ package bd.org.rain.camera;
 import android.Manifest;
 import android.app.Activity;
 import android.app.AlertDialog;
-
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
-
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.Settings;
-
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
@@ -28,15 +25,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
             if (getFromPref(this, ALLOW_KEY)) {
                 showSettingsAlert();
             } else if (ContextCompat.checkSelfPermission(this,
                     Manifest.permission.CAMERA)
-
                     != PackageManager.PERMISSION_GRANTED) {
-
                 // Should we show an explanation?
                 if (ActivityCompat.shouldShowRequestPermissionRationale(this,
                         Manifest.permission.CAMERA)) {
@@ -51,7 +45,6 @@ public class MainActivity extends AppCompatActivity {
         } else {
             openCamera();
         }
-
     }
     public static void saveToPreferences(Context context, String key, Boolean allowed) {
         SharedPreferences myPrefs = context.getSharedPreferences(CAMERA_PREF,
@@ -60,18 +53,15 @@ public class MainActivity extends AppCompatActivity {
         prefsEditor.putBoolean(key, allowed);
         prefsEditor.commit();
     }
-
     public static Boolean getFromPref(Context context, String key) {
         SharedPreferences myPrefs = context.getSharedPreferences(CAMERA_PREF,
                 Context.MODE_PRIVATE);
         return (myPrefs.getBoolean(key, false));
     }
-
     private void showAlert() {
         AlertDialog alertDialog = new AlertDialog.Builder(MainActivity.this).create();
         alertDialog.setTitle("Alert");
         alertDialog.setMessage("App needs to access the Camera.");
-
         alertDialog.setButton(AlertDialog.BUTTON_NEGATIVE, "DONT ALLOW",
                 new DialogInterface.OnClickListener() {
                     public void onClick(DialogInterface dialog, int which) {
@@ -79,10 +69,8 @@ public class MainActivity extends AppCompatActivity {
                         finish();
                     }
                 });
-
         alertDialog.setButton(AlertDialog.BUTTON_POSITIVE, "ALLOW",
                 new DialogInterface.OnClickListener() {
-
                     public void onClick(DialogInterface dialog, int which) {
                         dialog.dismiss();
                         ActivityCompat.requestPermissions(MainActivity.this,
@@ -92,46 +80,37 @@ public class MainActivity extends AppCompatActivity {
                 });
         alertDialog.show();
     }
-
     private void showSettingsAlert() {
         AlertDialog alertDialog = new AlertDialog.Builder(MainActivity.this).create();
         alertDialog.setTitle("Alert");
         alertDialog.setMessage("App needs to access the Camera.");
-
         alertDialog.setButton(AlertDialog.BUTTON_NEGATIVE, "DONT ALLOW",
                 new DialogInterface.OnClickListener() {
-
                     public void onClick(DialogInterface dialog, int which) {
                         dialog.dismiss();
                         //finish();
                     }
                 });
-
         alertDialog.setButton(AlertDialog.BUTTON_POSITIVE, "SETTINGS",
                 new DialogInterface.OnClickListener() {
-
                     public void onClick(DialogInterface dialog, int which) {
                         dialog.dismiss();
                         startInstalledAppDetailsActivity(MainActivity.this);
                     }
                 });
-
         alertDialog.show();
     }
-
     @Override
     public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
         switch (requestCode) {
             case MY_PERMISSIONS_REQUEST_CAMERA: {
                 for (int i = 0, len = permissions.length; i < len; i++) {
                     String permission = permissions[i];
-
                     if (grantResults[i] == PackageManager.PERMISSION_DENIED) {
                         boolean
                                 showRationale =
                                 ActivityCompat.shouldShowRequestPermissionRationale(
                                         this, permission);
-
                         if (showRationale) {
                             showAlert();
                         } else if (!showRationale) {
@@ -146,22 +125,18 @@ public class MainActivity extends AppCompatActivity {
                     }
                 }
             }
-
             // other 'case' lines to check for other
             // permissions this app might request
         }
     }
-
     @Override
     protected void onResume() {
         super.onResume();
     }
-
     public static void startInstalledAppDetailsActivity(final Activity context) {
         if (context == null) {
             return;
         }
-
         final Intent i = new Intent();
         i.setAction(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
         i.addCategory(Intent.CATEGORY_DEFAULT);
@@ -171,7 +146,6 @@ public class MainActivity extends AppCompatActivity {
         i.addFlags(Intent.FLAG_ACTIVITY_EXCLUDE_FROM_RECENTS);
         context.startActivity(i);
     }
-
     private void openCamera() {
         Intent intent = new Intent("android.media.action.IMAGE_CAPTURE");
         startActivity(intent);
